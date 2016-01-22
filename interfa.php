2f2f422359a53fe48eae01a5ee79ce8484d40d10
root <gerhardt87@126.com>
2016-01-22 20:54:08 +0800

    first

---
+++ interfa.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Gerhardt;
+
+  class interfa
+  {
+    public function subscribeHdl()
+    {
+      return "welcome";
+    }
+
+    public function textHdl()
+    {
+      return ", I am too busy now. Talk to me later, please.";
+    }
+
+    public function imageHdl(){ return "I do not understand image."; }
+    public function videoHdl(){ return "I do not understand video."; }
+    public function shortHdl(){ return "I do not understand video."; }
+    public function locatHdl(){ return "I do not understard location."; }
+
+    public function clickHdl(){ return "building now."; }
+//    public function 
+  }
+?>
